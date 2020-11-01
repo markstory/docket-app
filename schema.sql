d97e8f9a9b26aa2bc2d2699cb3f85124a97a14d2
Mark Story <mark@mark-story.com>
2020-10-31 22:36:51 -0400

    Initial commit.

---
+++ schema.sql
@@ -0,0 +1,65 @@
+CREATE TABLE users (
+    id int not null auto_increment primary key,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    created timestamp not null default current_timestamp,
+    modified timestamp not null on update current_timestamp,
+);
+
+-- Could add organizations and teams. Then projects would be owned
+-- by teams inside organizations.
+
+CREATE TABLE projects (
+    id int not null auto_increment primary key,
+    user_id int not null,
+    name varchar(255) not null,
+    slug varchar(255) not null,
+    color varchar(6) not null,
+    favorite boolean not null default 0,
+    archived boolean not null default 0,
+    ranking int not null default 0,
+    foreign key (user_id) references users(id)
+);
+
+CREATE TABLE todo_items (
+    id int not null auto_increment primary key,
+    project_id integer not null,
+    title text,
+    body text,
+    due_on date,
+    completed boolean not null default 0,
+    foreign key (project_id) references projects(id)
+);
+
+CREATE TABLE todo_comments (
+    id int not null auto_increment primary key,
+    todo_item_id integer not null,
+    user_id integer not null,
+    body text,
+    foreign key (user_id) references users(id),
+    foreign key (todo_item_id) references todo_items(id)
+);
+
+CREATE TABLE todo_subtasks (
+    id int not null auto_increment primary key,
+    todo_item_id integer not null,
+    title text,
+    body text,
+    foreign key (todo_item_id) references todo_items(id)
+);
+
+CREATE TABLE todo_labels (
+    id int not null auto_increment primary key,
+    project_id int not null,
+    label varchar(50) not null,
+    color char(6) not null
+    foreign key (project_id) references projects(id)
+);
+
+CREATE TABLE todo_items_todo_labels (
+    todo_item_id int not null,
+    todo_label_id int not null,
+    foreign key (todo_item_id) references todo_items(id),
+    foreign key (todo_label_id) references todo_labels(id),
+    primary key (todo_item_id, todo_label_id)
+);
