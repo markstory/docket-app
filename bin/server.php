acf89ed0c9ebe2a5eed1b450181fccfccce52a03
Mark Story <mark@mark-story.com>
2020-11-22 23:18:06 -0500

    Fix forking

--- bin/server.php
+++ bin/server.php
@@ -12,11 +12,23 @@ foreach ($servers as $server) {
     if ($pid === -1) {
         die('Could not fork');
     }
+    if ($pid) {
+        $pids[] = $pid;
+    }
     if (!$pid) {
-        // In child process.
-        pcntl_signal(SIGTERM, function () {
-            exit(0);
-        });
+        // Start shell task.
         shell_exec(implode(' ', array_map('escapeshellarg', $server)));
     }
+}
+
+while (count($pids) > 0) {
+    foreach ($pids as $i => $pid) {
+        $res = pcntl_waitpid($pid, $status, WNOHANG);
+
+        // Child has already exited.
+        if ($res == -1 || $res > 0) {
+            unset($pids[$i]);
+        }
+    }
+    sleep(1);
 }
