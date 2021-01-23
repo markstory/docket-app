d466097983b320fd7d36cc85d5a4ec71261494e4
Mark Story <mark@mark-story.com>
2021-01-23 00:38:57 -0500

    Add migrations.

---
+++ config/Migrations/20210123053705_Initial.php
@@ -0,0 +1,394 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up()
+    {
+        $this->table('labels')
+            ->addColumn('project_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('label', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false,
+            ])
+            ->addColumn('color', 'char', [
+                'default' => null,
+                'limit' => 6,
+                'null' => false,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'project_id',
+                ]
+            )
+            ->create();
+
+        $this->table('labels_tasks', ['id' => false, 'primary_key' => ['task_id', 'label_id']])
+            ->addColumn('task_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('label_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'label_id',
+                ]
+            )
+            ->addIndex(
+                [
+                    'task_id',
+                ]
+            )
+            ->create();
+
+        $this->table('projects')
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('slug', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('color', 'integer', [
+                'default' => '1',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('favorite', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('archived', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('ranking', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('incomplete_task_count', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'user_id',
+                ]
+            )
+            ->create();
+
+        $this->table('subtasks')
+            ->addColumn('task_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('title', 'text', [
+                'default' => null,
+                'limit' => 16777215,
+                'null' => true,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => 16777215,
+                'null' => true,
+            ])
+            ->addColumn('ranking', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('completed', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'task_id',
+                ]
+            )
+            ->create();
+
+        $this->table('tasks')
+            ->addColumn('project_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('title', 'text', [
+                'default' => null,
+                'limit' => 4294967295,
+                'null' => true,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => 4294967295,
+                'null' => true,
+            ])
+            ->addColumn('due_on', 'date', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('child_order', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('day_order', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('completed', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('subtask_count', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('complete_subtask_count', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'project_id',
+                ]
+            )
+            ->create();
+
+        $this->table('users')
+            ->addColumn('email', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('email_verified', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => '',
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('unverified_email', 'string', [
+                'default' => '',
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('timezone', 'string', [
+                'default' => 'UTC',
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('labels')
+            ->addForeignKey(
+                'project_id',
+                'projects',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'RESTRICT',
+                ]
+            )
+            ->update();
+
+        $this->table('labels_tasks')
+            ->addForeignKey(
+                'label_id',
+                'labels',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE',
+                ]
+            )
+            ->addForeignKey(
+                'task_id',
+                'tasks',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE',
+                ]
+            )
+            ->update();
+
+        $this->table('projects')
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'RESTRICT',
+                ]
+            )
+            ->update();
+
+        $this->table('subtasks')
+            ->addForeignKey(
+                'task_id',
+                'tasks',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE',
+                ]
+            )
+            ->update();
+
+        $this->table('tasks')
+            ->addForeignKey(
+                'project_id',
+                'projects',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE',
+                ]
+            )
+            ->update();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down()
+    {
+        $this->table('labels')
+            ->dropForeignKey(
+                'project_id'
+            )->save();
+
+        $this->table('labels_tasks')
+            ->dropForeignKey(
+                'label_id'
+            )
+            ->dropForeignKey(
+                'task_id'
+            )->save();
+
+        $this->table('projects')
+            ->dropForeignKey(
+                'user_id'
+            )->save();
+
+        $this->table('subtasks')
+            ->dropForeignKey(
+                'task_id'
+            )->save();
+
+        $this->table('tasks')
+            ->dropForeignKey(
+                'project_id'
+            )->save();
+
+        $this->table('labels')->drop()->save();
+        $this->table('labels_tasks')->drop()->save();
+        $this->table('projects')->drop()->save();
+        $this->table('subtasks')->drop()->save();
+        $this->table('tasks')->drop()->save();
+        $this->table('users')->drop()->save();
+    }
+}
