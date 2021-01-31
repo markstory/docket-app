709e722cd8c5d1f159c2e756552a5cf5374deebc
Mark Story <mark@mark-story.com>
2021-01-30 22:28:57 -0500

    Run eslint.

--- assets/js/locale.ts
+++ assets/js/locale.ts
@@ -4,7 +4,7 @@ const PLACEHOLDER_PATTERN = /\{(\w+)\}/;
  * Stub function until a proper localization library is chosen.
  */
 export function t(message: string, data?: Record<string, string | number>): string {
-  return message.replace(PLACEHOLDER_PATTERN, function(_match, key) {
+  return message.replace(PLACEHOLDER_PATTERN, function (_match, key) {
     if (data?.hasOwnProperty(key)) {
       return String(data[key]);
     }
