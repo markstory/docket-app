55ed41fd00ebbff14837aa25248dc2ac7d6b59f8
Mark Story <mark@mark-story.com>
2021-03-13 23:24:36 -0500

    Fix date suggestions.

--- assets/js/components/smartTaskInput.tsx
+++ assets/js/components/smartTaskInput.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import {MentionsInput, Mention} from 'react-mentions';
-import {addDays} from 'date-fns';
+import {addDays, parse} from 'date-fns';
 
 import {t} from 'app/locale';
 import {Project} from 'app/types';
-import {InlineIcon} from '@iconify/react';
-import {getToday, toDateString} from 'app/utils/dates';
+import {getToday, parseDateInput, parseDate, toDateString} from 'app/utils/dates';
 
 type Props = {
   defaultValue?: string;
@@ -14,31 +13,62 @@ type Props = {
   onChangeProject: (value: number) => void;
 };
 
+type MentionOption = {id: string; display: string};
+
+function generateMonth(name: string, end: number): MentionOption[] {
+  const today = getToday();
+  const options: MentionOption[] = [];
+  for (var i = 1; i <= end; i++) {
+    const value = `${name} ${i}`;
+    const time = parseDateInput(value);
+    options.push({id: toDateString(time ?? today), display: value});
+  }
+  return options;
+}
+
+function generateDateOptions(today: string): MentionOption[] {
+  const date = parseDate(today);
+  return [
+    {id: 's:' + today, display: t('Today')},
+    {id: 's:' + toDateString(addDays(date, 1)), display: t('Tomorrow')},
+    {id: 'r:' + toDateString(parseDateInput('Monday') ?? date), display: 'Monday'},
+    {id: 'r:' + toDateString(parseDateInput('Tuesday') ?? date), display: 'Tuesday'},
+    {id: 'r:' + toDateString(parseDateInput('Wednesday') ?? date), display: 'Wednesday'},
+    {id: 'r:' + toDateString(parseDateInput('Thursday') ?? date), display: 'Thursday'},
+    {id: 'r:' + toDateString(parseDateInput('Saturday') ?? date), display: 'Saturday'},
+    {id: 'r:' + toDateString(parseDateInput('Sunday') ?? date), display: 'Sunday'},
+    {id: 'r:' + toDateString(parseDateInput('Friday') ?? date), display: 'Friday'},
+    ...generateMonth('January', 31),
+    // TODO leap years.
+    ...generateMonth('February', 28),
+    ...generateMonth('March', 31),
+    ...generateMonth('April', 30),
+    ...generateMonth('May', 31),
+    ...generateMonth('June', 30),
+    ...generateMonth('July', 31),
+    ...generateMonth('August', 31),
+    ...generateMonth('September', 30),
+    ...generateMonth('October', 31),
+    ...generateMonth('November', 30),
+    ...generateMonth('December', 31),
+  ];
+}
+
 function SmartTaskInput({
   defaultValue,
   projects,
   onChangeDate,
   onChangeProject,
 }: Props): JSX.Element {
-  const [textValue, setTextValue] = useState(defaultValue ?? ''):
+  const [textValue, setTextValue] = useState(defaultValue ?? '');
   const [value, setValue] = useState(defaultValue ?? '');
+  const today = toDateString(getToday());
+  const dateOptions = useMemo(() => generateDateOptions(today), [today]);
 
-  function handleChange(_, newValue: string) {
+  function handleChange(_: any, newValue: string) {
     setValue(newValue);
     const newPlainText = newValue.replace(/#[^#]+#/, '').replace(/%[^%]%/, '');
     setTextValue(newPlainText);
-  }
-
-  function generateDateOptions(query) {
-    const today = getToday();
-    // TODO Figure out how to handle months.
-    if (!query) {
-      return [
-        {id: toDateString(today), display: t('Today')},
-        {id: toDateString(addDays(today, 1)), display: t('Tomorrow')},
-      ]
-    }
-    return [];
   }
 
   return (
@@ -49,22 +79,25 @@ function SmartTaskInput({
         value={value}
         onChange={handleChange}
         singleLine
+        allowSpaceInQuery
       >
         <Mention
           className="project-mention"
           trigger="#"
           displayTransform={(_id, display) => `#${display}`}
           markup="#__display__:__id__#"
-          onAdd={(id) => onChangeProject(Number(id))}
+          onAdd={id => onChangeProject(Number(id))}
           data={projects.map(project => ({id: project.id, display: project.name}))}
+          appendSpaceOnAdd
         />
         <Mention
           className="date-mention"
           trigger="%"
           displayTransform={(_id, display) => `%${display}`}
           markup="%__display__:__id__%"
-          onAdd={(id) => onChangeDate(String(id))}
-          data={generateDateOptions}
+          onAdd={id => onChangeDate(String(id).replace(/[sr]:/, ''))}
+          data={dateOptions}
+          appendSpaceOnAdd
         />
       </MentionsInput>
       <input type="hidden" name="title" value={textValue} />
