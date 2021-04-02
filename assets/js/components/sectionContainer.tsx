aec0664667eb76ebfd1e611d688fb4630c5556cd
Mark Story <mark@mark-story.com>
2021-04-01 23:52:05 -0400

    Future proof types

--- assets/js/components/sectionContainer.tsx
+++ assets/js/components/sectionContainer.tsx
@@ -53,7 +53,7 @@ function SectionContainer({active, children, id, project, section}: SectionProps
   });
   const style = {
     transform: CSS.Transform.toString(transform),
-    transition,
+    transition: transition ?? undefined,
   };
   const className = classnames('section-container', {
     'dnd-ghost': id === activeId,
