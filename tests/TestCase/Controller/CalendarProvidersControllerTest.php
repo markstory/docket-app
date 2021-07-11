6b78a39d0fcc10b53f6944a4a8900766f6b3350f
Mark Story <mark@mark-story.com>
2021-07-10 21:53:10 -0400

    Add tests for calendar providers controller.

--- tests/TestCase/Controller/CalendarProvidersControllerTest.php
+++ tests/TestCase/Controller/CalendarProvidersControllerTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace App\Test\TestCase\Controller;
 
 use App\Controller\CalendarProvidersController;
+use App\Test\TestCase\FactoryTrait;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\IntegrationTestTrait;
 use Cake\TestSuite\TestCase;
 
@@ -14,6 +16,7 @@ use Cake\TestSuite\TestCase;
  */
 class CalendarProvidersControllerTest extends TestCase
 {
+    use FactoryTrait;
     use IntegrationTestTrait;
 
     /**
@@ -22,10 +25,23 @@ class CalendarProvidersControllerTest extends TestCase
      * @var array
      */
     protected $fixtures = [
-        'app.CalendarProviders',
-        'app.Users',
         'app.CalendarSources',
+        'app.CalendarProviders',
+        'app.Projects',
+        'app.Users',
     ];
+
+    /**
+     * @var \App\Model\Table\CalendarProvidersTable
+     */
+    protected $CalendarProviders;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->CalendarProviders = TableRegistry::get('CalendarProviders');
+    }
 
     /**
      * Test index method
@@ -34,7 +50,17 @@ class CalendarProvidersControllerTest extends TestCase
      */
     public function testIndex(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        // Owned by a different user.
+        $this->makeCalendarProvider(2, 'other@example.com');
+        $ownProvider = $this->makeCalendarProvider(1, 'owner@example.com');
+
+        $this->login();
+        $this->get('/calendars');
+        $this->assertResponseOk();
+        $records = $this->viewVariable('calendarProviders')->toArray();
+
+        $this->assertCount(1, $records);
+        $this->assertEquals($ownProvider->id, $records[0]->id);
     }
 
     /**
@@ -44,27 +70,26 @@ class CalendarProvidersControllerTest extends TestCase
      */
     public function testView(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $provider = $this->makeCalendarProvider(1, 'owner@example.com');
+
+        $this->login();
+        $this->get("/calendars/{$provider->id}/view");
+        $this->assertResponseOk();
+        $this->assertNotNull($this->viewVariable('calendarProvider'));
     }
 
     /**
-     * Test add method
+     * Test view method
      *
      * @return void
      */
-    public function testAdd(): void
+    public function testViewPermissions(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
+        $provider = $this->makeCalendarProvider(2, 'other@example.com');
 
-    /**
-     * Test edit method
-     *
-     * @return void
-     */
-    public function testEdit(): void
-    {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->login();
+        $this->get("/calendars/{$provider->id}/view");
+        $this->assertResponseCode(403);
     }
 
     /**
@@ -74,6 +99,26 @@ class CalendarProvidersControllerTest extends TestCase
      */
     public function testDelete(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $provider = $this->makeCalendarProvider(1, 'owner@example.com');
+
+        $this->login();
+        $this->enableCsrfToken();
+        $this->post("/calendars/{$provider->id}/delete");
+        $this->assertRedirect('/calendars');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDeletePermissions(): void
+    {
+        $provider = $this->makeCalendarProvider(2, 'other@example.com');
+
+        $this->login();
+        $this->enableCsrfToken();
+        $this->post("/calendars/{$provider->id}/delete");
+        $this->assertResponseCode(403);
     }
 }
