bbe3460162058d7945c07af55cde23c67ab3cf84
Mark Story <mark@mark-story.com>
2021-08-19 10:03:43 -0400

    Fix phpcs errors.

--- src/Service/CalendarService.php
+++ src/Service/CalendarService.php
@@ -252,7 +252,11 @@ class CalendarService
                         foreach ($results as $event) {
                             $instances = [$event];
                             if (!empty($event->getRecurrence())) {
-                                $instances = $calendar->events->instances($source->provider_id, $event->id, $instanceOpts);
+                                $instances = $calendar->events->instances(
+                                    $source->provider_id,
+                                    $event->id,
+                                    $instanceOpts
+                                );
                             }
                             foreach ($instances as $instance) {
                                 $this->syncEvent($source, $instance);
