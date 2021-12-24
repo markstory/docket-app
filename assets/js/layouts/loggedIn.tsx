320db97ac3a4be9314916d4b67955e1a6d6dcb9a
Mark Story <mark@mark-story.com>
2021-12-23 22:11:54 -0500

    Adding automatic reloads so that the app feels more fluid

--- assets/js/layouts/loggedIn.tsx
+++ assets/js/layouts/loggedIn.tsx
@@ -1,4 +1,4 @@
-import {Fragment, useEffect, useState} from 'react';
+import {Fragment, useEffect, useState, useRef} from 'react';
 import {Inertia} from '@inertiajs/inertia';
 import {usePage} from '@inertiajs/inertia-react';
 
@@ -25,9 +25,12 @@ type Props = {
   children: React.ReactNode;
   title?: string;
 };
+// 30 Minutes
+const VISIBILITY_TIMEOUT = 60 * 30 * 1000;
 
 function LoggedIn({children, title}: Props) {
   const {projects, identity} = usePage().props as SharedPageProps;
+  const hiddenTime = useRef(0);
 
   useEffect(() => {
     if (title) {
@@ -42,6 +45,29 @@ function LoggedIn({children, title}: Props) {
       document.body.classList.add('theme-dark');
     }
   }, [title, identity]);
+
+  // Reload the application if data could be stale.
+  // A simple timeout is a rough solution. A more refined
+  // solution would be to keep track of 'revision number' or timestamp
+  // of when the page was loaded and then check if the server
+  // has a newer timestamp/revision.
+  useEffect(() => {
+    function listener() {
+      if (document.visibilityState === 'hidden') {
+        hiddenTime.current = Date.now();
+      }
+      if (document.visibilityState === 'visible') {
+        const now = Date.now();
+        if (now - hiddenTime.current > VISIBILITY_TIMEOUT) {
+          Inertia.reload();
+        }
+      }
+    }
+    window.addEventListener('visibilitychange', listener);
+    return function cleanup() {
+      window.removeEventListener('visibilitychange', listener);
+    };
+  }, []);
 
   const generationId = projects.reduce((acc, project) => {
     const currentId = [project.name, project.color, project.incomplete_task_count];
