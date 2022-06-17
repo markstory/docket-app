df0ebe56154ffbbd07315bc2087634b1168baaa5
Mark Story <mark@mark-story.com>
2022-06-16 23:53:35 -0400

    Switch to localstorage based database instead of sqlite
    While I love SQLite it presents a few problems around testing and
    maintaining schema. I think a simple keyvalue store is going to work
    well enough.

--- flutterapp/lib/database.dart
+++ flutterapp/lib/database.dart
@@ -1,95 +1,44 @@
 import 'dart:developer' as developer;
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:json_cache/json_cache.dart';
+import 'package:localstorage/localstorage.dart';
 
 import 'package:docket/models/apitoken.dart';
 import 'package:docket/models/task.dart';
 
 class LocalDatabase {
   // Configuration
-  static const String dbFileName = 'docket-local.sqlite';
+  static const String dbName = 'docket-localstorage';
 
-  // Table Constants.
-  static const String apiTokensTable = 'api_tokens';
-  static const String todayTasksTable = 'today_tasks';
+  static const String apiTokenKey = 'v1:apitoken';
+  static const String todayTasksKey = 'v1:todaytasks';
 
-  Database? _database;
-
-  Future<Database> database() async {
+  JsonCache? _database;
+  JsonCache database() {
     if (_database != null) {
       return _database!;
     }
-    _database = await _initDb(dbFileName);
+    _database = _initDb();
     return _database!;
   }
 
-  Future<Database> _initDb(String filePath) async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
-
-    // For when schema gets broken.
-    //await deleteDatabase(path);
-    return await openDatabase(path, version: 1,
-      onCreate: _createDb
-    );
-  }
-
-  Future<void> _createDb(Database db, int version) async {
-    developer.log('Creating Database');
-    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
-    const intType = 'INTEGER';
-    const textType = 'TEXT';
-    const requiredTextType = 'TEXT NOT NULL';
-
-    await db.execute('''
-CREATE TABLE $apiTokensTable (
-  id $idType,
-  token $requiredTextType,
-  last_used DATETIME
-)
-  ''');
-    await db.execute('''
-CREATE TABLE $todayTasksTable (
-  id $idType,
-  project_slug $textType,
-  project_name $textType,
-  project_color $int,
-  section_id $intType,
-  title $textType,
-  body $textType,
-  due_on DATETIME,
-  child_order $intType,
-  day_order $intType,
-  evening boolean,
-  completed boolean
-)
-    ''');
-  }
-
-  Future<void> close() async {
-    final db = await database();
-
-    db.close();
+  _initDb() {
+    final LocalStorage storage = LocalStorage(dbName);
+    return JsonCacheMem(JsonCacheLocalStorage(storage));
   }
 
   // ApiToken methods.
   Future<ApiToken> createApiToken(ApiToken apiToken) async {
-    final db = await database();
-
-    // Fixate the id so we do upserts.
-    apiToken = apiToken.copy(id: 1);
-    await db.insert(apiTokensTable, apiToken.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+    final db = database();
+    await db.refresh(apiTokenKey, apiToken.toMap());
 
     return apiToken;
   }
 
   Future<ApiToken?> fetchApiToken() async {
-    final db = await database();
-    var result = await db.query(apiTokensTable,
-      limit: 1
-    );
-    if (result.isNotEmpty) {
-      return ApiToken.fromMap(result.first);
+    final db = database();
+    var result = await db.value(apiTokenKey);
+    if (result != null) {
+      return ApiToken.fromMap(result);
     }
     return null;
   }
@@ -97,13 +46,14 @@ CREATE TABLE $todayTasksTable (
   // Task Loader Methods.
   /// Fetch all records in the 'today' view store.
   Future<List<Task>> fetchTodayTasks() async {
-    final db = await database();
-    var results = await db.query(todayTasksTable,
-      orderBy: 'due_on ASC, evening ASC, day_order ASC, title ASC'
-    );
-    if (results.isNotEmpty) {
+    final db = database();
+    var results = await db.value(todayTasksKey);
+    if (results == null) {
+      return [];
+    }
+    if (results['tasks'] != null) {
       List<Task> tasks = [];
-      for (var item in results) {
+      for (var item in results['tasks']) {
         tasks.add(Task.fromMap(item));
       }
       return tasks;
@@ -113,21 +63,15 @@ CREATE TABLE $todayTasksTable (
 
   /// Add records to the 'today' view store.
   Future<void> insertTodayTasks(List<Task> tasks) async {
-    final db = await database();
-    await db.transaction((txn) async {
-      for (var task in tasks) {
-        await txn.insert(
-          todayTasksTable, 
-          task.toMap(), 
-          conflictAlgorithm: ConflictAlgorithm.replace
-        );
-      }
+    final db = database();
+    await db.refresh(todayTasksKey, {
+      'tasks': tasks.map((task) => task.toMap()).toList(),
     });
   }
 
   /// Erase all rows in the 'today' view store.
   Future<void>clearTodayTasks() async {
-    final db = await database();
-    await db.delete(todayTasksTable);
+    final db = database();
+    await db.remove(todayTasksKey);
   }
 }
