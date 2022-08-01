c6aca9a196f73adcd588d6c15bfa64e5e2ba2fc5
Mark Story <mark@mark-story.com>
2022-07-31 23:25:38 -0400

    Add overdue section to Today view.

--- flutterapp/lib/screens/today.dart
+++ flutterapp/lib/screens/today.dart
@@ -25,6 +25,7 @@ class TodayScreen extends StatefulWidget {
 
 class _TodayScreenState extends State<TodayScreen> {
   List<TaskSortMetadata> _taskLists = [];
+  TaskSortMetadata? _overdue;
 
   @override
   void initState() {
@@ -58,6 +59,18 @@ class _TodayScreenState extends State<TodayScreen> {
               }
 
               if (_taskLists.isEmpty) {
+                var overdueTasks = data.tasks.where((task) => task.dueOn?.isBefore(today) ?? false).toList();
+                if (overdueTasks.isNotEmpty) {
+                  _overdue = TaskSortMetadata(
+                    icon: Icon(Icons.warning_outlined, color: customColors.actionDelete),
+                    title: 'Overdue',
+                    tasks: overdueTasks,
+                    onReceive: (Task task, int newIndex) {
+                      throw 'Cannot move task to overdue';
+                    }
+                  );
+                }
+
                 // No setState() as we don't want to re-render.
                 var todayTasks = TaskSortMetadata(
                   icon: Icon(Icons.today, color: customColors.dueToday),
@@ -66,10 +79,15 @@ class _TodayScreenState extends State<TodayScreen> {
                   calendarItems: data.calendarItems,
                   tasks: data.tasks.where((task) => !task.evening).toList(),
                   onReceive: (Task task, int newIndex) {
+                    var updates = {'evening': false, 'day_order': newIndex};
                     task.evening = false;
                     task.dayOrder = newIndex;
 
-                    return {'evening': false, 'day_order': newIndex};
+                    if (task.dueOn?.isBefore(today) ?? false) {
+                      task.dueOn = today;
+                      updates['due_on'] = formatters.dateString(today);
+                    }
+                    return updates;
                   }
                 );
 
@@ -80,16 +98,22 @@ class _TodayScreenState extends State<TodayScreen> {
 
                   tasks: data.tasks.where((task) => task.evening).toList(),
                   onReceive: (Task task, int newIndex) {
-                    task.evening = false;
+                    var updates = {'evening': true, 'day_order': newIndex};
+                    task.evening = true;
                     task.dayOrder = newIndex;
 
-                    return {'evening': true, 'day_order': newIndex};
+                    if (task.dueOn?.isBefore(today) ?? false) {
+                      task.dueOn = today;
+                      updates['due_on'] = formatters.dateString(today);
+                    }
+                    return updates;
                   }
                 );
+
                 _taskLists..add(todayTasks)..add(eveningTasks);
               }
 
-              return DragAndDropLists(
+              var dragList = DragAndDropLists(
                 children: _taskLists.map((taskListMeta) {
                   return DragAndDropList(
                     header: taskListMeta.renderHeader(theme),
@@ -105,6 +129,7 @@ class _TodayScreenState extends State<TodayScreen> {
                     }).toList(),
                   );
                 }).toList(),
+                itemDecorationWhileDragging: itemDragBoxDecoration(theme),
                 itemDragOnLongPress: true,
                 onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex, int newListIndex) async {
                   var task = _taskLists[oldListIndex].tasks[oldItemIndex];
@@ -125,37 +150,107 @@ class _TodayScreenState extends State<TodayScreen> {
                 onListReorder:(int oldIndex, int newIndex) {
                   throw 'List reordering not supported';
                 },
-                itemDecorationWhileDragging: itemDragBoxDecoration(theme),
+                onItemAdd: (DragAndDropItem newItem, int listIndex, int itemIndex) async {
+                  if (_overdue == null) {
+                    throw 'Should not receive items when _overdue is null';
+                  }
+
+                  // Calculate position of adding to a end.
+                  // Generally this will be zero but it is possible to add to the
+                  // bottom of a populated list too.
+                  var targetList = _taskLists[listIndex];
+                  if (itemIndex == -1) {
+                    itemIndex = targetList.tasks.length;
+                  }
+
+                  var itemChild = newItem.child as TaskItem;
+                  var task = itemChild.task;
+
+                  // Get the changes that need to be made on the server.
+                  var updates = _taskLists[listIndex].onReceive(task, itemIndex);
+                  setState(() {
+                    _overdue?.tasks.remove(task);
+                    _taskLists[listIndex].tasks.insert(itemIndex, task);
+                  });
+
+                  print('moving task ${task.title} updates $updates');
+                  // Update the moved task and reload from server async
+                  await tasksProvider.move(task, updates);
+                  tasksProvider.fetchToday();
+                }
               );
+
+              List<Widget> children = [];
+              if (_overdue != null) {
+                children.add(
+                  Flexible(
+                    flex: 2,
+                    child: buildOverdue(_overdue!, theme, customColors),
+                  )
+                );
+              }
+
+              children.add(
+                Flexible(
+                  flex: 10,
+                  child: dragList,
+                )
+              );
+
+              return Column(children: children);
             },
           ),
         );
       }
     );
   }
-}
 
+  Widget buildOverdue(TaskSortMetadata taskMeta, ThemeData theme, DocketColors customColors) {
+    return Column(
+      children: [
+        taskMeta.renderHeader(theme),
+        ...taskMeta.tasks.map((task) {
+          var taskItem = TaskItem(
+           task: task,
+           showDate: false,
+           showProject: true
+          );
+          return Draggable<DragAndDropItem>(
+            feedback: SizedBox(
+              width: 300,
+              height: 60,
+              child: Material(child: taskItem)
+            ),
+            data: DragAndDropItem(child: taskItem),
+            child: taskItem,
+          );
+        }).toList()
+      ]
+    );
+  }
+}
 
 // TODO find a better home for this view data object. Perhaps it and rendering it can be extracted
 // into a widget that takes a list of these?
 /// Metadata container for building sortable task lists.
 class TaskSortMetadata {
-  // Icon to show on the left of the heading.
+
+  /// Icon to show on the left of the heading.
   Widget? icon;
 
-  // Title shown in large bold type.
+  /// Title shown in large bold type.
   String? title;
 
-  // Title shown smaller with an underline.
+  /// Title shown smaller with an underline.
   String? subtitle;
 
-  // Header button shown after title. Can also be a Row
-  // if more than one button is required.
+  /// Header button shown after title. Can also be a Row
+  /// if more than one button is required.
   Widget? button;
 
-  List<Task> tasks = [];
+  List<Task> tasks;
 
-  List<CalendarItem> calendarItems = [];
+  List<CalendarItem> calendarItems;
 
   /// Called when a task is moved into this list.
   /// Expected to return the map of data that needs to be sent to the server.
