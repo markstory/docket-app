00dc5f066655233c1b25ebc1bcb131d9aad3dc5b
Mark Story <mark@mark-story.com>
2022-12-21 10:46:56 -0500

    Add coverage for datetime formatters

--- flutterapp/test/formatters_test.dart
+++ flutterapp/test/formatters_test.dart
@@ -52,4 +52,42 @@ void main() {
       expect(result, equals(expected));
     });
   });
+
+  group('formatters.paddedTime()', () {
+    test('it formats', () {
+      var today = DateTime.now();
+      var result = formatters.paddedTime(today);
+      var expected = DateFormat('Hm').format(today);
+      expect(result, equals(expected));
+    });
+  });
+
+  group('formatters.timeAgo()', () {
+    test('formats values', () {
+      var today = DateTime.now();
+      var eightDays = today.subtract(const Duration(days: 8));
+      expect(formatters.timeAgo(eightDays), equals('1 week ago'));
+
+      var threeDays = today.subtract(const Duration(days: 3));
+      expect(formatters.timeAgo(threeDays), equals('3 days ago'));
+
+      var yesterday = today.subtract(const Duration(days: 1));
+      expect(formatters.timeAgo(yesterday), equals('Yesterday'));
+
+      var hoursAgo = today.subtract(const Duration(hours: 2));
+      expect(formatters.timeAgo(hoursAgo), equals('2 hours ago'));
+
+      var hourAgo = today.subtract(const Duration(hours: 1));
+      expect(formatters.timeAgo(hourAgo), equals('An hour ago'));
+
+      var minutesAgo = today.subtract(const Duration(minutes: 10));
+      expect(formatters.timeAgo(minutesAgo), equals('10 minutes ago'));
+
+      var minuteAgo = today.subtract(const Duration(minutes: 1));
+      expect(formatters.timeAgo(minuteAgo), equals('A minute ago'));
+
+      var secondsAgo = today.subtract(const Duration(seconds: 10));
+      expect(formatters.timeAgo(secondsAgo), equals('10 seconds ago'));
+    });
+  });
 }
