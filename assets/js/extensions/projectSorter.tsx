53f40a841face7b6038aef339bba937f191121f4
Mark Story <mark@mark-story.com>
2023-10-08 22:01:23 -0400

    Remove retained sortable objects

--- assets/js/extensions/projectSorter.tsx
+++ assets/js/extensions/projectSorter.tsx
@@ -2,21 +2,18 @@ import htmx from 'htmx.org';
 import Sortable from 'sortablejs';
 
 (function () {
-  const sorters: Sortable[] = [];
-
   htmx.defineExtension('project-sorter', {
     onEvent: function (name, evt) {
       if (name !== 'htmx:afterProcessNode') {
         return;
       }
       const element = evt.target as HTMLElement;
-      const sorter = new Sortable(element, {
+      new Sortable(element, {
         animation: 150,
         ghostClass: 'dnd-ghost',
         dragClass: 'dnd-item-dragging',
         handle: '.dnd-handle',
       });
-      sorters.push(sorter);
     },
   });
 })();
