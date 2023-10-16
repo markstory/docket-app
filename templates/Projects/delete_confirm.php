2b03d34db8bb7f463f0ae9edd3354a1e15ee80bf
Mark Story <mark@mark-story.com>
2023-10-15 21:22:32 -0400

    Extract confirm dialog into an element.
    There are a few of these in the app

--- templates/Projects/delete_confirm.php
+++ templates/Projects/delete_confirm.php
@@ -5,27 +5,8 @@ declare(strict_types=1);
  */
 $this->setLayout('ajax');
 
-$target = $this->Url->build(['_name' => 'projects:delete', 'slug' => $project->slug]);
-
-// TODO figure out how to close the modal on click.
-?>
-<div class="confirm-overlay">
-    <dialog class="confirm-dialog" open="true">
-        <?= $this->Form->create(null, ['url' => $target]) ?>
-        <h2>Are you sure?</h2>
-        <p>This will delete all the tasks in this project as well.</p>
-        <div class="button-bar-right">
-            <?= $this->Form->button('Cancel', [
-                // TOOD need to close the modal?
-                'class' => 'button button-muted',
-                'data-testid' => 'confirm-cancel'
-            ]) ?>
-            <?= $this->Form->button('Ok', [
-                'type' => 'submit',
-                'class' => 'button button-danger',
-                'data-testid' => 'confirm-proceed'
-            ]) ?>
-        </div>
-        <?= $this->Form->end() ?>
-    </dialog>
-</div>
+echo $this->element('confirm_dialog', [
+    'target' => ['_name' => 'projects:delete', 'slug' => $project->slug],
+    'title' => 'Are you sure?',
+    'description' => 'This will delete all tasks in this project as well.',
+]);
---
+++ templates/element/confirm_dialog.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+/**
+ * A generic confirmation dialog.
+ *
+ * @var string $target The url to submit to
+ * @var string $title The title of the dialog
+ * @var string $description Body copy
+ */
+
+// TODO figure out how to close the modal on click.
+?>
+<div class="confirm-overlay">
+    <dialog class="confirm-dialog" open="true">
+        <?= $this->Form->create(null, ['url' => $target]) ?>
+        <h2><?= $title ?></h2>
+        <p><?= $description ?></p>
+        <div class="button-bar-right">
+            <?= $this->Form->button('Cancel', [
+                // TOOD need to close the modal?
+                'class' => 'button button-muted',
+                'data-testid' => 'confirm-cancel'
+            ]) ?>
+            <?= $this->Form->button('Ok', [
+                'type' => 'submit',
+                'class' => 'button button-danger',
+                'data-testid' => 'confirm-proceed'
+            ]) ?>
+        </div>
+        <?= $this->Form->end() ?>
+    </dialog>
+</div>
