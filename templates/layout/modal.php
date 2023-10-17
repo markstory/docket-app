66139e303ba1667c9246cdced4745ae8c1d9bd69
Mark Story <mark@mark-story.com>
2023-10-16 23:26:33 -0700

    Remove redundant element

--- templates/layout/modal.php
+++ templates/layout/modal.php
@@ -2,6 +2,5 @@
 declare(strict_types=1);
 ?>
 <modal-window closeable="<?= h($closeable ?? 'true') ?>" open="<?=h($open) ?>">
-    <div class="modal-overlay"></div>
     <?= $this->fetch('content') ?>
 </modal-window>
