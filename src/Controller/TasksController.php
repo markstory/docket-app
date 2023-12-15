e988fcfaff6b410c09f470a80c2c38891cdaa297
Mark Story <mark@mark-story.com>
2023-12-14 23:01:38 -0500

    Make tasks/add work on its own.

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -203,6 +203,7 @@ class TasksController extends AppController
         // TODO add support for query string default values.
         $task = $this->Tasks->newEntity($this->request->getQueryParams());
         $task->subtasks = [];
+        $task->evening ??= false;
 
         $success = true;
         $redirect = null;
--- templates/Tasks/add.php
+++ templates/Tasks/add.php
@@ -6,13 +6,20 @@ declare(strict_types=1);
  * @var \App\Model\Entity\ProjectSection[] $sections
  * @var string $referer
  */
-$this->set('closable', true);
-$this->set('open', true);
-$this->setLayout('modal');
+$isHtmx = $this->request->is('htmx');
+
+$this->setLayout('sidebar');
+if ($isHtmx) {
+    $this->set('closable', true);
+    $this->set('open', true);
+    $this->setLayout('modal');
+}
 
 $this->assign('title', 'New Task');
-?>
+
+if ($isHtmx) : ?>
 <dialog class="task-add">
+<?php endif ?>
     <h2>Create a Task</h2>
     <button class="modal-close" modal-close="true">&#x2715;</button>
     <?= $this->element('task_form', [
@@ -22,4 +29,6 @@ $this->assign('title', 'New Task');
         'referer' => $referer,
         'url' => ['_name' => 'tasks:add'],
     ]); ?>
+<?php if ($isHtmx) : ?>
 </dialog>
+<?php endif ?>
