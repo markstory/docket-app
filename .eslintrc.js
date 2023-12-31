49f91191aa8ee8e56e6b0333693daf9251e71439
Mark Story <mark@mark-story.com>
2023-12-30 22:57:49 -0500

    Cleanup eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,26 +7,12 @@ module.exports = {
       jsx: true,
     },
   },
-  settings: {
-    react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
-  },
   plugins: ['jest'],
   extends: [
-    'plugin:react/recommended',
     // Prettier rules need to be at the end as they tweak other plugins.
     'prettier',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
-    'prettier/react',
   ],
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    //
-    'react/prop-types': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
+  rules: {},
 };
