0cd5610790eb0816f21ac9913a03cf09a805a0cd
Mark Story <mark@mark-story.com>
2024-02-25 00:46:48 -0500

    Add API CalendarSourcesController and tests

---
+++ src/Controller/Api/CalendarSourcesController.php
@@ -0,0 +1,203 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Controller\AppController;
+use App\Model\Entity\CalendarSource;
+use App\Service\CalendarService;
+use Cake\View\JsonView;
+use RuntimeException;
+
+/**
+ * CalendarSources Controller
+ *
+ * @property \App\Model\Table\CalendarSourcesTable $CalendarSources
+ */
+class CalendarSourcesController extends AppController
+{
+    public function viewClasses(): array
+    {
+        return [JsonView::class];
+    }
+
+    protected function getSource(): CalendarSource
+    {
+        $query = $this->CalendarSources
+            ->find()
+            ->contain('CalendarProviders')
+            ->where([
+                // User id condition is applied with an authorization check.
+                'CalendarSources.calendar_provider_id' => $this->request->getParam('providerId'),
+                'CalendarSources.id' => $this->request->getParam('id'),
+            ]);
+
+        /** @var \App\Model\Entity\CalendarSource */
+        return $query->firstOrFail();
+    }
+
+    protected function urlToProvider($providerId = null)
+    {
+        $providerId = $providerId ?? $this->request->getParam('providerId');
+
+        return [
+            '_name' => 'calendarproviders:index',
+            '?' => [
+                'provider' => $providerId,
+            ],
+        ];
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function add(CalendarService $service, $providerId = null)
+    {
+        $provider = $this->CalendarSources->CalendarProviders->get($providerId, [
+            'contain' => ['CalendarSources'],
+        ]);
+        $this->Authorization->authorize($provider, 'edit');
+        $serialize = [];
+        $success = false;
+        $error = '';
+
+        if ($this->request->is('post')) {
+            $data = $this->request->getData();
+            $data['calendar_provider_id'] = $providerId;
+
+            $source = $this->CalendarSources->newEntity($data);
+            if ($this->CalendarSources->save($source)) {
+                $serialize = ['source'];
+                $this->set('source', $source);
+
+                $service->setAccessToken($provider);
+                try {
+                    $service->createSubscription($source);
+                    $success = true;
+                } catch (RuntimeException $e) {
+                    $error = __('Your calendar was added but will not automatically synchronize.');
+                }
+            } else {
+                $error = __('Could not add that calendar.');
+            }
+        }
+        if ($error) {
+            $serialize[] = 'error';
+            $this->set('error', $error);
+        }
+
+        return $this->respond([
+            'success' => $success,
+            'flashSuccess' => __('Your calendar was added and will be synced.'),
+            'flashError' => $error,
+            'serialize' => $serialize,
+            'redirect' => $this->urlToProvider($provider->id),
+        ]);
+    }
+
+    public function sync(CalendarService $service)
+    {
+        $source = $this->getSource();
+        $this->Authorization->authorize($source->calendar_provider, 'sync');
+
+        // Force a resync and clear the local database. This could result in
+        // a blank calendar list should the sync fail. While I could design around
+        // this, I want to see if it happens first.
+        $source->sync_token = null;
+        $this->CalendarSources->CalendarItems->deleteAll(['calendar_source_id' => $source->id]);
+
+        $service->setAccessToken($source->calendar_provider);
+        $success = true;
+        $serialize = [];
+        try {
+            $service->syncEvents($source);
+            $this->set('source', $source);
+            $serialize[] = 'source';
+        } catch (\Exception $e) {
+            $success = false;
+            $error = __('Calendar not refreshed. %s', $e->getMessage());
+            $this->set('errors', [$error]);
+        }
+
+        return $this->respond([
+            'success' => $success,
+            'serialize' => $serialize,
+        ]);
+    }
+
+    /**
+     * Edit method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit()
+    {
+        $calendarSource = $this->getSource();
+        $this->Authorization->authorize($calendarSource->calendar_provider);
+
+        $serialize = [];
+        $success = false;
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            // Only a subset of fields are user editable.
+            $calendarSource = $this->CalendarSources->patchEntity($calendarSource, $this->request->getData(), [
+                'fields' => ['color', 'name'],
+            ]);
+            if ($this->CalendarSources->save($calendarSource)) {
+                $success = true;
+                $serialize = ['source'];
+                $this->set('source', $calendarSource);
+            } else {
+                $serialize = ['errors'];
+                $this->set('errors', $this->flattenErrors($calendarSource->getErrors()));
+            }
+        }
+
+        return $this->respond([
+            'success' => $success,
+            'serialize' => $serialize,
+            'flashSuccess' => __('The calendar has been updated.'),
+            'flashError' => __('The calendar could not be modified. Please, try again.'),
+            'redirect' => $this->urlToProvider($calendarSource->calendar_provider_id),
+        ]);
+    }
+
+    /**
+     * Delete method
+     *
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete(CalendarService $service)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $calendarSource = $this->getSource();
+        $this->Authorization->authorize($calendarSource->calendar_provider);
+
+        $service->setAccessToken($calendarSource->calendar_provider);
+        $service->cancelSubscriptions($calendarSource);
+
+        $success = true;
+        if (!$this->CalendarSources->delete($calendarSource)) {
+            $success = false;
+        }
+
+        return $this->respond([
+            'success' => $success,
+            'flashSuccess' => __('Calendar deleted'),
+            'flashError' => __('Calendar not deleted. Please try again.'),
+            'redirect' => $this->urlToProvider($calendarSource->calendar_provider_id),
+        ]);
+    }
+
+    public function deleteConfirm()
+    {
+        $calendarSource = $this->getSource();
+        $this->Authorization->authorize($calendarSource->calendar_provider, 'edit');
+
+        $this->set('calendarSource', $calendarSource);
+    }
+}
---
+++ tests/TestCase/Controller/Api/CalendarSourcesControllerTest.php
@@ -0,0 +1,364 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use App\Test\TestCase\FactoryTrait;
+use Cake\I18n\FrozenTime;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\Api\CalendarSourcesController Test Case
+ */
+class CalendarSourcesControllerTest extends TestCase
+{
+    use FactoryTrait;
+    use IntegrationTestTrait;
+
+    /**
+     * @var string[]
+     */
+    protected $fixtures = [
+        'app.Users',
+        'app.CalendarProviders',
+        'app.CalendarSources',
+        'app.CalendarSubscriptions',
+        'app.CalendarItems',
+        'app.Projects',
+    ];
+
+    /**
+     * @var \App\Model\Table\CalendarItemsTable
+     */
+    protected $CalendarItems;
+
+    /**
+     * @var \App\Model\Table\CalendarSourcesTable
+     */
+    protected $CalendarSources;
+
+    /**
+     * @var \App\Model\Table\UsersTable
+     */
+    protected $Users;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->Users = $this->fetchTable('Users');
+        $this->CalendarSources = $this->fetchTable('CalendarSources');
+        $this->CalendarItems = $this->fetchTable('CalendarItems');
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        FrozenTime::setTestNow(null);
+    }
+
+    /**
+     * @vcr controller_calendarsources_sync.yml
+     */
+    public function testSync(): void
+    {
+        FrozenTime::setTestNow('2021-07-11 12:13:14');
+
+        $provider = $this->makeCalendarProvider(1, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id, 'primary', [
+            'provider_id' => 'calendar-1',
+        ]);
+        $this->loginApi(1);
+
+        $this->post("/api/calendars/{$provider->id}/sources/{$source->id}/sync");
+
+        $this->assertResponseOk();
+        $result = $this->CalendarItems->find()->where([
+            'CalendarItems.calendar_source_id' => $source->id,
+        ])->toArray();
+        $this->assertCount(3, $result);
+        foreach ($result as $event) {
+            $this->assertNotEmpty($event->title);
+            $this->assertNotEmpty($event->html_link);
+            $this->assertNotEmpty($event->getStart());
+            $this->assertNotEmpty($event->getEnd());
+        }
+
+        $source = $this->CalendarSources->get($source->id);
+        $this->assertSame('next-sync-token', $source->sync_token);
+    }
+
+    /**
+     * @vcr controller_calendarsources_sync.yml
+     */
+    public function testSyncApi(): void
+    {
+        FrozenTime::setTestNow('2021-07-11 12:13:14');
+
+        $token = $this->makeApiToken(1);
+        $provider = $this->makeCalendarProvider(1, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id, 'primary', [
+            'provider_id' => 'calendar-1',
+        ]);
+
+        $this->useApiToken($token->token);
+        $this->requestJson();
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/sync");
+
+        $source = $this->viewVariable('source');
+        $this->assertNotEmpty($source);
+
+        $result = $this->CalendarItems->find()->where([
+            'CalendarItems.calendar_source_id' => $source->id,
+        ])->toArray();
+        $this->assertCount(3, $result);
+        foreach ($result as $event) {
+            $this->assertNotEmpty($event->title);
+            $this->assertNotEmpty($event->html_link);
+            $this->assertNotEmpty($event->getStart());
+            $this->assertNotEmpty($event->getEnd());
+        }
+
+        $source = $this->CalendarSources->get($source->id);
+        $this->assertSame('next-sync-token', $source->sync_token);
+    }
+
+    /**
+     * @vcr controller_calendarsources_sync.yml
+     */
+    public function testSyncReplaceExistingRemoveDeleted(): void
+    {
+        FrozenTime::setTestNow('2021-07-11 12:13:14');
+
+        $provider = $this->makeCalendarProvider(1, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id, 'primary', [
+            'provider_id' => 'calendar-1',
+        ]);
+        $update = $this->makeCalendarItem($source->id, [
+            'title' => 'old title',
+            'provider_id' => 'calendar-event-1',
+        ]);
+        $remove = $this->makeCalendarItem($source->id, [
+            'title' => 'remove',
+            'provider_id' => 'calendar-event-4',
+        ]);
+
+        $this->login();
+        $this->enableCsrfToken();
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/sync");
+        $this->assertRedirect("/calendars?provider={$provider->id}");
+        $this->assertFlashElement('flash/success');
+
+        $this->assertFalse($this->CalendarItems->exists(['id' => $remove->id]));
+
+        $updated = $this->CalendarItems->findById($update->id)->first();
+        $this->assertNull($updated);
+
+        $created = $this->CalendarItems->findByTitle('Dentist Appointment')->firstOrFail();
+        $this->assertNotNull($created);
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete(): void
+    {
+        $user = $this->Users->get(1);
+        $provider = $this->makeCalendarProvider($user->id, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id);
+
+        $this->login();
+        $this->enableCsrfToken();
+
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/delete");
+        $this->assertRedirect("/calendars?provider={$provider->id}");
+        $this->assertFalse($this->CalendarSources->exists(['CalendarSources.id' => $source->id]));
+    }
+
+    /**
+     * Test delete cancel subscription
+     *
+     * @vcr controller_calendarsources_delete.yml
+     * @return void
+     */
+    public function testDeleteCancelSubscription(): void
+    {
+        $user = $this->Users->get(1);
+        $provider = $this->makeCalendarProvider($user->id, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id);
+        $this->makeCalendarSubscription($source->id, 'subscription-id');
+
+        $this->login();
+        $this->enableCsrfToken();
+
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/delete");
+        $this->assertRedirect("/calendars?provider={$provider->id}");
+        $this->assertFalse($this->CalendarSources->exists(['CalendarSources.id' => $source->id]));
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDeleteNoPermission(): void
+    {
+        $user = $this->Users->get(2);
+        $provider = $this->makeCalendarProvider($user->id, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id);
+
+        $this->login();
+        $this->enableCsrfToken();
+
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/delete");
+        $this->assertResponseCode(403);
+    }
+
+    /**
+     * @vcr controller_calendarsources_add_post.yml
+     */
+    public function testAddPost()
+    {
+        $provider = $this->makeCalendarProvider(1, 'test@example.com');
+
+        $this->login();
+        $this->enableRetainFlashMessages();
+        $this->enableCsrfToken();
+
+        $this->post("/calendars/{$provider->id}/sources/add", [
+            'provider_id' => 'calendar-1',
+            'color' => 1,
+            'name' => 'Work Calendar',
+        ]);
+        $this->assertRedirect("/calendars?provider={$provider->id}");
+        $this->assertFlashElement('flash/success');
+
+        $source = $this->CalendarSources->findByName('Work Calendar')->firstOrFail();
+        $this->assertSame('calendar-1', $source->provider_id);
+
+        $subs = $this->fetchTable('CalendarSubscriptions');
+        $sub = $subs->findByCalendarSourceId($source->id)->firstOrFail();
+        $this->assertNotEmpty($sub->identifier);
+    }
+
+    /**
+     * @vcr controller_calendarsources_add_post.yml
+     */
+    public function testAddApiToken()
+    {
+        $token = $this->makeApiToken(1);
+        $provider = $this->makeCalendarProvider(1, 'test@example.com');
+
+        $this->useApiToken($token->token);
+        $this->requestJson();
+
+        $this->post("/calendars/{$provider->id}/sources/add", [
+            'provider_id' => 'calendar-1',
+            'color' => 1,
+            'name' => 'Work Calendar',
+        ]);
+        $this->assertResponseSuccess();
+
+        $source = $this->viewVariable('source');
+        $this->assertSame('calendar-1', $source->provider_id);
+
+        $subs = $this->fetchTable('CalendarSubscriptions');
+        $sub = $subs->findByCalendarSourceId($source->id)->firstOrFail();
+        $this->assertNotEmpty($sub->identifier);
+    }
+
+    /**
+     * @vcr controller_calendarsources_add_post_fail.yml
+     */
+    public function testAddPostSubscriptionFail()
+    {
+        $provider = $this->makeCalendarProvider(1, 'test@example.com');
+
+        $this->login();
+        $this->enableRetainFlashMessages();
+        $this->enableCsrfToken();
+
+        $this->post("/calendars/{$provider->id}/sources/add", [
+            'provider_id' => 'calendar-1',
+            'color' => 1,
+            'name' => 'Work Calendar',
+        ]);
+        $this->assertRedirect("/calendars?provider={$provider->id}");
+        $this->assertFlashElement('flash/error');
+
+        $source = $this->CalendarSources->findByName('Work Calendar')->firstOrFail();
+        $this->assertSame('calendar-1', $source->provider_id);
+
+        $subs = $this->fetchTable('CalendarSubscriptions');
+        $this->assertEmpty($subs->findByCalendarSourceId($source->id)->first());
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit(): void
+    {
+        $user = $this->Users->get(1);
+        $provider = $this->makeCalendarProvider($user->id, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id);
+
+        $this->login();
+        $this->enableCsrfToken();
+
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/edit", [
+            'color' => 3,
+            'name' => 'new values',
+        ]);
+        $this->assertRedirect("/calendars?provider={$provider->id}");
+        $this->assertFlashElement('flash/success');
+    }
+
+    /**
+     * Test edit with api token method
+     *
+     * @return void
+     */
+    public function testEditApi(): void
+    {
+        $token = $this->makeApiToken(1);
+        $user = $this->Users->get(1);
+        $provider = $this->makeCalendarProvider($user->id, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id);
+
+        $this->requestJson();
+        $this->useApiToken($token->token);
+
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/edit", [
+            'color' => 3,
+            'name' => 'new values',
+        ]);
+        $this->assertResponseOk();
+        $this->assertResponseContains('new values');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEditPermissions(): void
+    {
+        $user = $this->Users->get(2);
+        $provider = $this->makeCalendarProvider($user->id, 'test@example.com');
+        $source = $this->makeCalendarSource($provider->id);
+
+        $this->login();
+        $this->enableCsrfToken();
+
+        $this->post("/calendars/{$provider->id}/sources/{$source->id}/edit", [
+            'color' => 3,
+            'name' => 'new values',
+        ]);
+        $this->assertResponseCode(403);
+    }
+}
