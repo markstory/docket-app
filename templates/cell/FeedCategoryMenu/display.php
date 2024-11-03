eb8fa9af49ab9423bb25a2a12f446058d081f650
Mark Story <mark@mark-story.com>
2024-11-03 00:39:16 -0400

    Rough in category expansion

--- templates/cell/FeedCategoryMenu/display.php
+++ templates/cell/FeedCategoryMenu/display.php
@@ -22,14 +22,41 @@ declare(strict_types=1);
             <?= $this->element('icons/grabber24') ?>
         </button>
         <div>
-            <!-- item element needs a toggle for open/close the category -->
-            <?= $this->element('feed_category_item', ['feedCategory' => $category]) ?>
-            <!-- read the category.open and conditionally render the menu -->
-            <ul>
-            <?php foreach ($category->feed_subscriptions as $subscription) : ?>
-                <li><?= $this->Html->link($subscription->alias, ['_name' => 'feedsubscriptions:view', 'id' => $subscription->id]) ?></li>
-            <?php endforeach; ?>
-            </ul>
+            <?php
+            $viewUrl = $this->Url->build(['_name' => 'feedcategories:view', 'id' => $category->id]);
+            $toggleUrl = $this->Url->build(['_name' => 'feedcategories:toggleexpanded', 'id' => $category->id]);
+            ?>
+            <div class="feed-category-item">
+                <button
+                    role="button"
+                    class="button-icon"
+                    title="Expand category"
+                    hx-post="<?= $toggleUrl ?>"
+                    hx-target=".dnd-dropper-left-offset"
+                    hx-swap="outerHTML"
+                >
+                    <?php if ($category->expanded) : ?>
+                        <?= $this->element('icons/chevron16') ?>
+                    <?php else : ?>
+                        <?= $this->element('icons/chevron-right16') ?>
+                    <?php endif; ?>
+                </button>
+                <a href="<?= $viewUrl ?>" hx-boost="1">
+                    <span class="feed-category-badge">
+                        <?= $this->element('icons/directory16', ['color' => $category->color_hex]) ?>
+                        <span><?= h($category->title) ?></span>
+                    </span>
+                </a>
+                <span class="counter"><?= h($category->unread_item_count ?? 99) ?></span>
+            </div>
+
+            <?php if ($category->expanded) : ?>
+                <ul>
+                <?php foreach ($category->feed_subscriptions as $subscription) : ?>
+                    <li><?= $this->Html->link($subscription->alias, ['_name' => 'feedsubscriptions:view', 'id' => $subscription->id]) ?></li>
+                <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
         </div>
     </div>
 <?php endforeach; ?>
