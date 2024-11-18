98f0492bb1521161863a7872d1ad12dfd90febca
Mark Story <mark@mark-story.com>
2024-11-17 23:49:44 -0500

    Update feedsubscription delete flow as well.

--- templates/FeedSubscriptions/delete_confirm.php
+++ templates/FeedSubscriptions/delete_confirm.php
@@ -2,13 +2,8 @@
 declare(strict_types=1);
 /**
  * @var \App\Model\Entity\FeedSubscription $feedSubscription
+ * @var \Cake\View\View $this
  */
-// configure layout
-$this->set('closable', false);
-$this->set('open', true);
-
-$this->setLayout('modal');
-
 echo $this->element('confirm_dialog', [
     'target' => ['_name' => 'feedsubscriptions:delete', 'id' => $feedSubscription->id],
     'title' => 'Are you sure?',
