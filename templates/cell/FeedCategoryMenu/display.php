41fb1c5f2aa9c6d63f2a21e98a976e5b7bea7eaf
Mark Story <mark@mark-story.com>
2024-12-16 00:32:05 -0500

    Style the category menu a bit.

--- templates/cell/FeedCategoryMenu/display.php
+++ templates/cell/FeedCategoryMenu/display.php
@@ -21,49 +21,50 @@ declare(strict_types=1);
         <button class="dnd-handle" role="button" aria-roledescription="sortable">
             <?= $this->element('icons/grabber24') ?>
         </button>
-        <div>
-            <?php
-            $viewUrl = $this->Url->build(['_name' => 'feedcategories:view', 'id' => $category->id]);
-            $toggleUrl = $this->Url->build(['_name' => 'feedcategories:toggleexpanded', 'id' => $category->id]);
-            ?>
-            <div class="feed-category-item">
-                <button
-                    role="button"
-                    class="button-icon"
-                    title="Expand category"
-                    hx-post="<?= $toggleUrl ?>"
-                    hx-target=".dnd-dropper-left-offset"
-                    hx-swap="outerHTML"
-                >
-                    <?php if ($category->expanded) : ?>
-                        <?= $this->element('icons/chevron16') ?>
-                    <?php else : ?>
-                        <?= $this->element('icons/chevron-right16') ?>
-                    <?php endif; ?>
-                </button>
-                <a href="<?= $viewUrl ?>" hx-boost="1">
-                    <span class="feed-category-badge">
-                        <?= $this->element('icons/directory16', ['color' => $category->color_hex]) ?>
-                        <span><?= h($category->title) ?></span>
-                    </span>
-                </a>
-                <span class="counter"><?= h($category->unread_item_count ?? 99) ?></span>
-            </div>
-
-            <?php if ($category->expanded) : ?>
-                <ul>
-                <?php foreach ($category->feed_subscriptions as $subscription) : ?>
-                <li>
-                    <?php if ($subscription->feed?->favicon_url) : ?>
-                        <?= $this->Html->image($subscription->feed->favicon_url, ['width' => 16, 'height' => 16]) ?>
-                    <?php endif; ?>
-                    <?= $this->Html->link($subscription->alias, ['_name' => 'feedsubscriptions:view', 'id' => $subscription->id]) ?>
-                    <span class="counter"><?= h($subscription->unread_item_count ?? 91) ?></span>
-                </li>
-                <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
+        <?php
+        $viewUrl = $this->Url->build(['_name' => 'feedcategories:view', 'id' => $category->id]);
+        $toggleUrl = $this->Url->build(['_name' => 'feedcategories:toggleexpanded', 'id' => $category->id]);
+        ?>
+        <div class="feed-category-item">
+            <button
+                role="button"
+                class="button-icon"
+                title="Expand category"
+                hx-post="<?= $toggleUrl ?>"
+                hx-target=".dnd-dropper-left-offset"
+                hx-swap="outerHTML"
+            >
+                <?php if ($category->expanded) : ?>
+                    <?= $this->element('icons/chevron16') ?>
+                <?php else : ?>
+                    <?= $this->element('icons/chevron-right16') ?>
+                <?php endif; ?>
+            </button>
+            <a href="<?= $viewUrl ?>" hx-boost="1">
+                <span class="feed-category-badge">
+                    <?= $this->element('icons/directory16', ['color' => $category->color_hex]) ?>
+                    <span><?= h($category->title) ?></span>
+                </span>
+            </a>
+            <span class="counter"><?= h($category->unread_item_count ?? 99) ?></span>
         </div>
     </div>
+    <?php if ($category->expanded) : ?>
+        <ul class="feed-category-feeds">
+        <?php foreach ($category->feed_subscriptions as $subscription) : ?>
+        <li>
+            <span>
+                <?php if ($subscription->feed?->favicon_url) : ?>
+                    <?= $this->Html->image($subscription->feed->favicon_url, ['width' => 16, 'height' => 16]) ?>
+                <?php else: ?>
+                    <?= $this->element('icons/rss16', ['color' => $category->color_hex]) ?>
+                <?php endif; ?>
+                <?= $this->Html->link($subscription->alias, ['_name' => 'feedsubscriptions:view', 'id' => $subscription->id]) ?>
+            </span>
+            <span class="counter"><?= h($subscription->unread_item_count ?? 91) ?></span>
+        </li>
+        <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 <?php endforeach; ?>
 <?= $this->Form->end() ?>
