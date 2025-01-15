1a54234f9d0eab689bab7cd757f7874fda4b20eb
Mark Story <mark@mark-story.com>
2025-01-14 23:47:57 -0500

    Fix menu overflow for now.
    this isn't a great permanent solution, but it works well enough for now.
    Ideally dropdowns used portals to get on top of other content.

--- assets/js/webcomponents/modalWindow.ts
+++ assets/js/webcomponents/modalWindow.ts
@@ -1,9 +1,27 @@
 class ModalWindow extends HTMLElement {
   connectedCallback() {
     const dialog = this.querySelector('dialog');
+    if (!dialog) {
+      console.error('No dialog element found');
+      return;
+    }
+
     dialog.showModal();
 
     this.setupClose(dialog);
+
+    const selectbox = this.querySelector('select-box');
+    if (selectbox) {
+      // If there is a selectbox child, allow for the dropdown
+      // to overflow. This isn't a perfect solution either but works ok.
+      // in larger modals that have scroll, elements can overflow with this change.
+      selectbox.addEventListener('open', function () {
+        dialog.style.overflow = 'visible';
+      });
+      selectbox.addEventListener('close', function () {
+        dialog.style.overflow = 'auto';
+      });
+    }
   }
 
   setupClose(dialog: HTMLDialogElement | null): void {
