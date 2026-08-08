4209ca8e611de28865ed5b65e4626cb5e47c3819
Mark Story <mark@mark-story.com>
2026-08-08 18:22:40 -0400

    Fix more tests

--- flutterapp/test/screens/calendarproviderlist_test.dart
+++ flutterapp/test/screens/calendarproviderlist_test.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
 
 import 'package:docket/actions.dart' as actions;
 import 'package:docket/database.dart';
@@ -14,6 +16,7 @@ import 'package:docket/screens/calendarproviderlist.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  SharedPreferencesAsyncPlatform.instance = InMemorySharedPreferencesAsync.empty();
 
   var file = File('test_resources/calendar_provider_list.json');
   final calendarsResponse = file.readAsStringSync();
--- flutterapp/test/screens/projectcompleted_test.dart
+++ flutterapp/test/screens/projectcompleted_test.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:docket/models/apitoken.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
 
 import 'package:docket/database.dart';
 import 'package:docket/main.dart';
@@ -11,6 +13,7 @@ import 'package:docket/screens/projectcompleted.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  SharedPreferencesAsyncPlatform.instance = InMemorySharedPreferencesAsync.empty();
 
   var file = File('test_resources/project_details.json');
   final projectDetails = file.readAsStringSync();
