7992e3b4aad669fc0429df76d7012f882be8c959
Mark Story <mark@mark-story.com>
2026-09-06 21:09:08 -0400

    Forgot about these when updating tsx -> ts

--- templates/element/frontend_assets.php
+++ templates/element/frontend_assets.php
@@ -6,10 +6,10 @@ use Cake\Core\Configure;
 $debug = Configure::read('debug');
 ?>
 <?php if ($debug) : ?>
-    <?= $this->ViteAsset->css('assets/js/app.tsx') ?>
+    <?= $this->ViteAsset->css('assets/js/app.ts') ?>
     <?= $this->Html->script('http://localhost:3000/@vite/client', ['type' => 'module']) ?>
-    <?= $this->Html->script('http://localhost:3000/assets/js/app.tsx', ['type' => 'module']) ?>
+    <?= $this->Html->script('http://localhost:3000/assets/js/app.ts', ['type' => 'module']) ?>
 <?php else : ?>
-    <?= $this->ViteAsset->css('assets/js/app.tsx') ?>
-    <?= $this->ViteAsset->script('assets/js/app.tsx') ?>
+    <?= $this->ViteAsset->css('assets/js/app.ts') ?>
+    <?= $this->ViteAsset->script('assets/js/app.ts') ?>
 <?php endif;
